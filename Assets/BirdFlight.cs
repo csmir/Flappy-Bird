25f115b3719b4bda0329578ce1de5a31ce531614
Armano den Boef <68127614+Rozen4334@users.noreply.github.com>
2023-01-31 11:01:14 +0100

    Write flight script

--- Assets/BirdFlight.cs
+++ Assets/BirdFlight.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
 
 public class BirdFlight : MonoBehaviour
 {
+    public float Velocity = 0;
+    private Rigidbody2D _rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            _rb.velocity = (Vector2.up * Velocity);
+        }
     }
 }
